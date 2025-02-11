d695fc0bff86c4ea8e17e15d1cea769ffe1dbefa
suliman-btk <suliman56563@gmail.com>
2025-02-12 02:59:11 +0800

    maigra

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/run-migrations', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    Artisan::call('db:seed', ['--force' => true]); // Runs the seeder
+    return 'Migrations and seeders ran successfully!';
 });
